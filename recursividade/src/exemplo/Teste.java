128b2761e98d9af185c956923ce0fb2c0a998e20
brunnuscz <brunocruz012013@gmail.com>
2023-01-08 13:55:15 -0300

    Recursividade

--- recursividade/src/exemplo/Teste.java
+++ recursividade/src/exemplo/Teste.java
@@ -3,7 +3,7 @@ package exemplo;
 public class Teste {
 	public static void main(String[] agrs) {
 		int valor = 5;
-		System.out.print("Fibonacci dos "+valor+" numeros: ");
+		System.out.print("Fibonacci dos "+valor+" primeiros numeros: ");
 		for(int i=0; i<valor; i++) {
 			System.out.print(Calculadora.fibonacci(i)+" ");			
 		}
