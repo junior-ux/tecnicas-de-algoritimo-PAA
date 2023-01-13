24d83ceb3f24c6d9c8f926fc3d18d569a3affbce
junior-ux <55417250+junior-ux@users.noreply.github.com>
2023-01-13 12:22:28 -0300

    comentário do código

--- divisão-e-conquista/Insertion-sort.py
+++ divisão-e-conquista/Insertion-sort.py
@@ -1,27 +1,21 @@
-# Python program for implementation of Insertion Sort
- 
-# Function to do insertion sort
+
 def insertionSort(arr):
  
-    # Traverse through 1 to len(arr)
+    # percorre o array elemento por elemento
     for i in range(1, len(arr)):
  
         key = arr[i]
  
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
-        j = i-1
+        # Mover elementos da matriz [0..i-1], que são
+        # maior que a chave, para uma posição à frente
+        # da sua posição actual
+        j = i-1 # Volta ao elemento anterior para comparar com os anteriores 
         while j >= 0 and key < arr[j] :
                 arr[j + 1] = arr[j]
                 j -= 1
-        arr[j + 1] = key
- 
- 
-# Driver code to test above
-arr = [12, 11, 13, 5, 6]
+        arr[j + 1] = key # Atribue o valor de troca de posição temporariamente
+
+arr = [12, 11, 13, 5, 6] # array a ser testado acima
 insertionSort(arr)
 for i in range(len(arr)):
     print ("% d" % arr[i])
- 
-# This code is contributed by Mohit Kumra
