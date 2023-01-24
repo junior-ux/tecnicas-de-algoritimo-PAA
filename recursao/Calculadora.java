d65191c39ca72bd154c891a306b36355a8f4f5b7
Bruno <brunocruz012013@gmail.com>
2023-01-24 16:56:15 -0300

    Adicionando tecnica de recursão

---
+++ recursao/Calculadora.java
@@ -0,0 +1,11 @@
+
+public class Calculadora {
+	public static int fibonacci(int num) {
+		if(num == 0) {
+			return 0;
+		}else if(num == 1 || num == 2) {
+			return 1;
+		}
+		return fibonacci(num-1)+fibonacci(num-2);
+	}
+}
---
+++ recursao/Teste.java
@@ -0,0 +1,11 @@
+
+public class Teste {
+	public static void main(String[] agrs) {
+		int valor = 5;
+		System.out.print("Fibonacci dos "+valor+" primeiros numeros: ");
+		for(int i=0; i<valor; i++) {
+			System.out.print(Calculadora.fibonacci(i)+" ");			
+		}
+		System.out.println();
+	}
+}
